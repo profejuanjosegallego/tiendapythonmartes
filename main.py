da1efd0096e01a826eb711136401768e676180ae
fzdocente <fzdocente@gmail.com>
2025-03-11 20:53:32 -0500

    feat se crea rutina de busqueda y edicion de productos

--- main.py
+++ main.py
@@ -33,9 +33,25 @@ while opcion != 5:
         
         
     elif opcion==2:
-        print("estoy en la 2")
+       
+       #Utilizando ciclos FOR en python para recorrer LISTAS
+       for productoSeleccionado in productos:
+           print(productoSeleccionado["nombre"])
+       
     elif opcion==3:
-        print("estoy en la 3")
+        
+        #0. PREGUNTAR A  QUIEN QUERES EDITAR
+        productoCambio=int(input("Digita el id del producto a cambiar: "))
+        #1. ENCONTRAR EL ELEMENTO
+        for productoBuscado in productos:
+            if productoBuscado["id"]==productoCambio:
+                print("OE LO ENCONTRE")
+            else:
+                print("PARCE NO LO ENCONTRE")
+        #2. SELECCIONO EL ELEMENTO
+        #3. ACCEDO A LAS PROPIEDADES O ATRIBUTOS QUE
+        #QUIERO O PUEDO MODIFICAR
+        
     elif opcion==4:
         print("estoy en la 4")
     else:
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Juan Jose Gallego Mesa
+## Examen 1 de python (fundamentos logicos)
+### Medellin 2025
+#### Docente: MR Gallego
